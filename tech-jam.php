0b538d9ea2d124b168f65bc6b0297baa265b2f17
Generaalguan <33372@ma-web.nl>
2023-09-07 12:23:39 +0200

    zit vast

---
+++ tech-jam.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    // dit is een for loop die van 33 tot 543 telt
+    for ($i = 33; $i < 543; $i++) {
+
+        //hier wordt er gecheckt of de nummer even of oneven is
+        //even v
+        if ($i % 2 == 0) {
+            echo "<b> $i </b>" . "<br>";
+            //oneven v
+        } elseif ($i % 2 != 0) {
+            echo "$i " . "<br>";
+        } else {
+            echo "hoe is dit een nummer";
+        }
+
+        // hier wordt het gecheckt of ik het kan delen tussen 5,9 en 5&9
+        // gedeeld door 5 v
+        if ($i % 5 == 0) {
+            echo "Software" . "<br>";
+        }
+        // gedeeld door 9 v
+        if ($i % 9 == 0) {
+            echo "Developer" . "<br>";
+        }
+                // gedeeld door 5&9 v
+        if ($i % 5 == 0 && $i % 9 == 0) {
+            echo "Web" . "<br>";
+        }
+
+
+
+
+    }
+
+    ?>
+</body>
+
+</html>
